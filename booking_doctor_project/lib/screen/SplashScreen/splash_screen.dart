cb0a5020fc0b31574f9059f09f289146b0057866
hnhatnam <namzt1975@gmail.com>
2024-11-27 17:33:50 +0700

    Add LogIn and SignUp Screen

--- booking_doctor_project/lib/screen/SplashScreen/splash_screen.dart
+++ booking_doctor_project/lib/screen/SplashScreen/splash_screen.dart
@@ -1,43 +1,58 @@
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+import 'package:booking_doctor_project/utils/text_styles.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget {
+import '../../utils/localfiles.dart';
+import '../WelcomeScreen/welcome_screen.dart';
+
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text(
-        'Splash Screen',
-        style: TextStyle(fontSize: 30),
+    final size = MediaQuery.of(context).size;
+    return FlutterSplashScreen.scale(
+      gradient: const LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Colors.white,
+          Color.fromARGB(255, 212, 235, 248),
+          Color.fromARGB(255, 128, 196, 233),
+          Color.fromARGB(255, 31, 80, 154),
+          Color.fromARGB(255, 10, 57, 129),
+        ],
       ),
-    ));
-  }
-}
-
-import 'package:flutter/material.dart';
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text(
-        'Splash Screen',
-        style: TextStyle(fontSize: 30),
+      childWidget: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: size.width / 3.5,
+            child: Image.asset(
+              Localfiles.logo,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          SizedBox(
+            height: size.height * 0.01,
+          ),
+          Text(
+            'NhapVien',
+            style: TextStyles(context).getTitleStyle(size: 36),
+          ),
+          SizedBox(
+            height: size.height * 0.001,
+          ),
+          Text(
+            'Medical Booking System',
+            style: TextStyles(context)
+                .getTitleStyle(size: 18, fontWeight: FontWeight.w500),
+          ),
+        ],
       ),
-    ));
+      duration: const Duration(milliseconds: 2500),
+      animationDuration: const Duration(milliseconds: 2000),
+      nextScreen: const WelcomeScreen(),
+    );
   }
 }
