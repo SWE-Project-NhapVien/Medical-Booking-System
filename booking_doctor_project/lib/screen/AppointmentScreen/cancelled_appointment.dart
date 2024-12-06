5c932794dd4446df165221bbdecc91100fc13317
phuotato <pphuonganh.work@gmail.com>
2024-12-06 10:50:54 +0700

    .

--- booking_doctor_project/lib/screen/AppointmentScreen/cancelled_appointment.dart
+++ booking_doctor_project/lib/screen/AppointmentScreen/cancelled_appointment.dart
@@ -1,7 +1,6 @@
 import 'package:booking_doctor_project/bloc/Appointment/CancelledAppointment/cancelled_appointment_bloc.dart';
 import 'package:booking_doctor_project/bloc/Appointment/CancelledAppointment/cancelled_appointment_state.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../bloc/Appointment/CancelledAppointment/cancelled_appointment_event.dart';
