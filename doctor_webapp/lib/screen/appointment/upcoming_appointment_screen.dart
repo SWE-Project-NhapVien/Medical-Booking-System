441cbf5cd9c3969487637a06b3f07107be283aba
zHuskyz <nvk01052004@gmail.com>
2024-12-27 18:27:17 +0700

    80% doc web

--- doctor_webapp/lib/screen/appointment/upcoming_appointment_screen.dart
+++ doctor_webapp/lib/screen/appointment/upcoming_appointment_screen.dart
@@ -1,4 +1,7 @@
 import 'package:doctor_webapp/bloc/Appointment/appointment_state.dart';
+import 'package:doctor_webapp/bloc/SpecificAppointment/specific_appointment_bloc.dart';
+import 'package:doctor_webapp/bloc/SpecificAppointment/specific_appointment_event.dart';
+import 'package:doctor_webapp/bloc/SpecificAppointment/specific_appointment_state.dart';
 import 'package:doctor_webapp/screen/appointment/common_appointment_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -28,42 +31,61 @@ class _UpcomingAppointmentScreenState extends State<UpcomingAppointmentScreen> {
   @override
   Widget build(BuildContext context) {
     double lottieSize = MediaQuery.of(context).size.width * 0.2;
-    return BlocConsumer<AppointmentBloc, AppointmentState>(
-        listener: (context, state) {
-      if (state is LoadingAppointmentState) {
-        Center(
-          child: AlertDialog(
-            backgroundColor: Colors.transparent,
-            content: Lottie.asset(
-              FixedWebComponent.loading,
-              width: lottieSize,
-            ),
-          ),
-        );
-      } else if (state is ErrorAppointmentState) {
-        print(state.message);
-      }
-    }, builder: (context, state) {
-      if (state is SucessAppointmentState) {
-        final List<AppointmentCard> list = state.appointment
-            .map((e) => AppointmentCard(
-                  appointmentId: e.appointmentId,
-                  patientFullName: e.patientFullName,
-                  date: e.date,
-                  time: e.time,
-                  patientAvatarURL: e.patientAvatarURL ??
-                      FixedWebComponent.defaultPatientAvatar,
-                  onTap: () {},
-                ))
-            .toList();
-        return ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (context, index) {
-            return list[index];
-          },
-        );
-      }
-      return Container();
-    });
+    return BlocConsumer<GetSpecificAppointmentDataBloc,
+        GetSpecificAppointmentDataState>(
+      listener: (context, state2) {
+        if (state2 is GetSpecificAppointmentDataSuccess) {
+          setState(() {});
+        }
+        if (state2 is GetSpecificAppointmentDataError) {
+          print(state2.message);
+        }
+      },
+      builder: (context, state) {
+        return BlocConsumer<AppointmentBloc, AppointmentState>(
+            listener: (context, state) {
+          if (state is LoadingAppointmentState) {
+            Center(
+              child: AlertDialog(
+                backgroundColor: Colors.transparent,
+                content: Lottie.asset(
+                  FixedWebComponent.loading,
+                  width: lottieSize,
+                ),
+              ),
+            );
+          } else if (state is ErrorAppointmentState) {
+            print(state.message);
+          }
+        }, builder: (context, state) {
+          if (state is SucessAppointmentState) {
+            final List<AppointmentCard> list = state.appointment
+                .map((e) => AppointmentCard(
+                      appointmentId: e.appointmentId,
+                      patientFullName: e.patientFullName,
+                      date: e.date,
+                      time: e.time,
+                      patientAvatarURL: e.patientAvatarURL ??
+                          FixedWebComponent.defaultPatientAvatar,
+                      onTap: () {
+                        context
+                            .read<GetSpecificAppointmentDataBloc>()
+                            .add(GetSpecificAppointmentDataEvent(
+                              appointmentId: e.appointmentId,
+                            ));
+                      },
+                    ))
+                .toList();
+            return ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                return list[index];
+              },
+            );
+          }
+          return Container();
+        });
+      },
+    );
   }
 }
